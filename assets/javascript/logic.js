2116276128edf75ee198e5afc3424d85fe1d9446
Tyler Sweeney <tylersweeney@gmail.com>
2018-07-29 14:11:47 -0700

    create on click event for button to generate search results

---
+++ assets/javascript/logic.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    var animals = [
+        "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
+        "bird", "ferret", "turtle", "sugar glider", "chinchilla",
+        "hedgehog", "hermit crab", "gerbil", "pigmy goat", "chicken",
+        "capybara", "teacup pig", "serval", "salamander", "frog"
+    ];
+
+    // Function to make buttons and add to page
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (var i = 0; i < arrayToUse.length; i++) {
+            var a =$("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.text(arrayToUse[i]);
+            $(areaToAddTo).append(a);
+        }
+    
+    }
+
+    $(document).on("click", ".animal-button", function() {
+        $("#animals").empty();
+        $(".animal-button").removeClass("active");
+        $(this).addClass("active");
+
+        var type = $(this).attr("data-type");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=oM3BUOSenZ2VJomaFilz2gaTGRKk7P8C&limit=10";
+
+        $ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var animalDiv = $("<div class=\"animal-item\">");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+            }
+        })
+    })
+})
